f4a433f91b9ef56be405c465d59df7657d4317e2
roshan1209 <roshan1209britto@gmail.com>
2023-03-30 09:52:58 +0530

    shop_page

--- src/app/components/shop-page/shop-page.component.ts
+++ src/app/components/shop-page/shop-page.component.ts
@@ -6,27 +6,60 @@ import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-shop-page',
   templateUrl: './shop-page.component.html',
-  styleUrls: ['./shop-page.component.css']
+  styleUrls: ['./shop-page.component.css'],
 })
 export class ShopPageComponent implements OnInit {
-  cartDetail=[];
-  
-  constructor(private makeapi:ApiService,private router :Router,private http: HttpClient) { }
+  cartDetail = [];
+  cartlist = [];
+  collectiondata = [];
+
+  constructor(
+    private makeapi: ApiService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    this.data_create()
+  }
+  data_store(){
+    for(let i=0;i<=this.collectiondata.length;i++){
+      debugger
+      this.makeapi.getsubcollection(this.collectiondata[i]).subscribe((res) => {
+        debugger;
+        res.map((e: any) => {
+          this.cartDetail.push(e.payload.doc.data());
+        });
+      });
+    }
+    console.log(this.cartDetail);
+  }
+  data_create() {
+    if(this.collectiondata.length == 0){
+      debugger
+      this.makeapi.listItem('Business').subscribe( (res) => {
+        debugger;
+        this.cartlist = res.map((e: any) => {
+          // var collection = e.payload.doc.data();
+          this.collectiondata.push(e.payload.doc.id);
+          debugger;
+          console.log(this.collectiondata);
+        });
+        debugger
+        this.data_store()
+      });
+    }
+    else{
+      debugger
+      this.data_store();
+    }
     
-    this.http.get<any>('assets/JSON/cartdetail.json').subscribe(
-      data=>{
-        // console.log(data);
-        this.cartDetail = data;
-        console.log(this.cartDetail);
-      }
-    )
+    
   }
-  productview(ind:number){
-    this.router.navigate(['/productview'],{queryParams:{index:ind}})
+  productview(ind: number) {
+    this.router.navigate(['/productview'], { queryParams: { index: ind } });
   }
-  cartview(){
+  cartview() {
     this.router.navigate(['cart']);
   }
 }
